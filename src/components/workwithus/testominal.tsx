5329e046ba0dfaef2fb064de9e2041cd1b9a62c6
Muhammad-Adil-code <anybhai6@gnail.com>
2025-11-19 19:24:22 +0500

    commit changes

--- src/components/workwithus/testominal.tsx
+++ src/components/workwithus/testominal.tsx
@@ -5,91 +5,91 @@ import power from '../../images/power-black-flag.png'
 export default function testominal() {
   return (
     <>
-    <section className="section-testimonial-with-image">
-      <img src={power.src} alt="" />
-      <article className="testimonial bg-secondary">
-        <div className="quote">
-          <div className="quote__review">
-            <p />
-            <p>
-  “We achieved double- and triple-digit growth across all KPIs. Burac Ai’s team established a stable, predictable, and trackable lead funnel, resulting in a significant increase in lead volume.”
-</p>
+      <section className="section-testimonial-with-image">
+        <img src={power.src} alt="" />
+        <article className="testimonial bg-secondary">
+          <div className="quote">
+            <div className="quote__review">
+              <p />
+              <p>
+                “We achieved double- and triple-digit growth across all KPIs. Burac Ai’s team established a stable, predictable, and trackable lead funnel, resulting in a significant increase in lead volume.”
+              </p>
 
-            <p />
+              <p />
+            </div>
+            <div className="d-flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={17}
+                height={15}
+                viewBox="0 0 17 15"
+                fill="none"
+              >
+                <path
+                  d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
+                  fill="#7756ff"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={17}
+                height={15}
+                viewBox="0 0 17 15"
+                fill="none"
+              >
+                <path
+                  d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
+                  fill="#7756ff"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={17}
+                height={15}
+                viewBox="0 0 17 15"
+                fill="none"
+              >
+                <path
+                  d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
+                  fill="#7756ff"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={17}
+                height={15}
+                viewBox="0 0 17 15"
+                fill="none"
+              >
+                <path
+                  d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
+                  fill="#7756ff"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={17}
+                height={15}
+                viewBox="0 0 17 15"
+                fill="none"
+              >
+                <path
+                  d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
+                  fill="#7756ff"
+                />
+              </svg>
+            </div>
+            <div className="quote__author-and-logo">
+              <p className="quote-lauthor">VP Marketing, quadient</p>
+              <img
+                className="quote-logo"
+                src={quadient.src}
+                alt=""
+              />
+            </div>
           </div>
-          <div className="d-flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={17}
-              height={15}
-              viewBox="0 0 17 15"
-              fill="none"
-            >
-              <path
-                d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
-                fill="#7756ff"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={17}
-              height={15}
-              viewBox="0 0 17 15"
-              fill="none"
-            >
-              <path
-                d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
-                fill="#7756ff"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={17}
-              height={15}
-              viewBox="0 0 17 15"
-              fill="none"
-            >
-              <path
-                d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
-                fill="#7756ff"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={17}
-              height={15}
-              viewBox="0 0 17 15"
-              fill="none"
-            >
-              <path
-                d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
-                fill="#7756ff"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={17}
-              height={15}
-              viewBox="0 0 17 15"
-              fill="none"
-            >
-              <path
-                d="M8.5 0L10.8041 4.82865L16.1085 5.52786L12.2281 9.21135L13.2023 14.4721L8.5 11.92L3.79772 14.4721L4.77186 9.21135L0.891548 5.52786L6.19588 4.82865L8.5 0Z"
-                fill="#7756ff"
-              />
-            </svg>
-          </div>
-          <div className="quote__author-and-logo">
-            <p className="quote-lauthor">VP Marketing, quadient</p>
-            <img
-              className="quote-logo"
-              src={quadient.src}
-              alt=""
-            />
-          </div>
-        </div>
-      </article>
-    </section>
+        </article>
+      </section>
     </>
   )
 }
